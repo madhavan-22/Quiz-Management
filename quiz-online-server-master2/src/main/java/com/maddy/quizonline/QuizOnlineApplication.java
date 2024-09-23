ff679af7daddf7fe7740670d87954bad50c60573
madhavan-22 <maddy22madhav@gmail.com>
2024-09-23 16:43:35 +0530

    Java Files

---
+++ quiz-online-server-master2/src/main/java/com/maddy/quizonline/QuizOnlineApplication.java
@@ -0,0 +1,15 @@
+package com.maddy.quizonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication(scanBasePackages = "com.dailycodework.quizonline")
+@EntityScan(basePackages = "com.dailycodework.quizonline.model")
+public class QuizOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizOnlineApplication.class, args);
+    }
+
+}
---
+++ quiz-online-server-master2/src/main/java/com/maddy/quizonline/service/QuestionService.java
@@ -0,0 +1,73 @@
+package com.maddy.quizonline.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.maddy.quizonline.model.Question;
+import com.maddy.quizonline.repository.QuestionRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Simpson Alfred
+ */
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService implements IQuestionService {
+
+    private final QuestionRepository questionRepository;  // No need to initialize with null
+
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+    
+    @Override
+    public Question createQuestion(Question question) {
+        return questionRepository.save(question);
+    }
+
+    @Override
+    public List<Question> getAllQuestions() {
+        return questionRepository.findAll();
+    }
+
+    @Override
+    public Optional<Question> getQuestionById(Long id) {
+        return questionRepository.findById(id);
+    }
+
+    @Override
+    public List<String> getAllSubjects() {
+        return questionRepository.findDistinctSubject();
+    }
+
+    @Override
+    public Question updateQuestion(Long id, Question question) throws ChangeSetPersister.NotFoundException {
+        Optional<Question> theQuestion = this.getQuestionById(id);
+        if (theQuestion.isPresent()) {
+            Question updatedQuestion = theQuestion.get();
+            updatedQuestion.setQuestion(question.getQuestion());
+            updatedQuestion.setChoices(question.getChoices());
+            updatedQuestion.setCorrectAnswers(question.getCorrectAnswers());
+            return questionRepository.save(updatedQuestion);
+        } else {
+            throw new ChangeSetPersister.NotFoundException();
+        }
+    }
+
+    @Override
+    public void deleteQuestion(Long id) {
+        questionRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Question> getQuestionsForUser(Integer numOfQuestions, String subject) {
+        Pageable pageable = PageRequest.of(0, numOfQuestions);
+        return questionRepository.findBySubject(subject, pageable).getContent();
+    }
+}
